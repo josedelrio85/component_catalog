7db3ed9b9bd7f564c72f2383ca5eeaa1afc200c5
Ivan Perez Fuentes <ivanprzf@gmail.com>
2020-05-08 12:12:48 +0200

    Added component search bar
    This commit adds a component search bar to find components in an easier way.

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelectorAll('.copy-clipboard').forEach(div => {
         div.addEventListener('click', function() {
             navigator.clipboard.writeText(this.nextSibling.innerText)
-            .then(() => {  
+            .then(() => {
                 // Text copied to clipboard
                 //console.log('Text copied to clipboard');
             })
@@ -28,8 +28,54 @@ document.addEventListener("DOMContentLoaded", function (event) {
               // If the user denies clipboard permissions:
               console.error('Could not copy text: ', err);
             });
-        
+
         });
     });
 
 });
+
+//Search on press search button
+//Note: now it searchs in all the component html block. Maybe should change it to search just in component title (or a more descriptive field like a description or keywords)?
+//  document.addEventListener('click', function (event) {
+//     if (event.target.matches('#submit-search')) {
+//         event.preventDefault();
+//         var termsearch = document.getElementById("term-search").value;
+//         if(termsearch && termsearch.length <= 3) return;
+//         //Get all components
+//         var components = document.getElementsByClassName("single-component");
+
+//         //Loop through components
+//         for (var i = 0; i < components.length; i++) {
+//             if(!termsearch || components.item(i).innerHTML.search(termsearch) != -1) {
+//                 //Show element if string is in its content
+//                 components.item(i).classList.remove('d-none');
+//             } else {
+//                 //Hide element if string is not in its content
+//                 components.item(i).classList.add('d-none');
+//             }
+//         }
+//     }
+//   }, false);
+
+//Search on keyup event
+//Note: now it searchs in all the component html block. Maybe should change it to search just in component title (or a more descriptive field like a description or keywords)?
+document.addEventListener('keyup', function (event) {
+    if (event.target.matches('#term-search')) {
+        event.preventDefault();
+        var termsearch = document.getElementById("term-search").value;
+        if(termsearch && termsearch.length <= 3) return;
+        //Get all components
+        var components = document.getElementsByClassName("single-component");
+
+        //Loop through components
+        for (var i = 0; i < components.length; i++) {
+            if(!termsearch || components.item(i).innerHTML.search(termsearch) != -1) {
+                //Show element if string is in its content
+                components.item(i).classList.remove('d-none');
+            } else {
+                //Hide element if string is not in its content
+                components.item(i).classList.add('d-none');
+            }
+        }
+    }
+}, false);
